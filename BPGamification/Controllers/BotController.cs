7b7ff63d2d60855d484edeadeff6a08050136985
Geluos <rodion.bezuglov@yandex.ru>
2021-09-05 01:18:15 +0300

    Proto BotController

---
+++ BPGamification/Controllers/BotController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BPGamification;
+using Microsoft.EntityFrameworkCore;
+using BPGamification.Interfaces;
+
+namespace Bot.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinShopController : ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public CoinShopController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        [HttpGet("getWork/[body]")]
+        public async Task<ActionResult> GetWork(WorksHistory data)
+        {
+            try
+            {
+                await repository.AddWorkHistory(data);
+                if(data.Status==true)
+                {
+                    Coin cn = await repository.GetUserCoins((int)data.UserId);
+                    cn.Raiting += 5;
+                    await repository.ChangeUserCoin(cn);
+                }
+                //Проверка не выполнились ли задания..
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "database problems while getting Personal Page");
+            }
+        }
+
+
+    }
+}
